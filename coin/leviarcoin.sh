8193cdbc64054398a4148459d4c58ff1018fc059
secrectvn <quoclong.hacker1412@gmail.com>
2018-01-20 20:30:30 +0700

    Update leviarcoin.sh

--- coin/leviarcoin.sh
+++ coin/leviarcoin.sh
@@ -6,10 +6,10 @@ sudo apt-get install software-properties-common ;
 sudo apt-get install git build-essential cmake libuv1-dev libmicrohttpd-dev htop screen -y ;
 sudo git clone https://github.com/xmrig/xmrig.git mining && mkdir -p ~/mining/build && cd ~/mining/build && cmake .. && make ;
 cd ~/mining/build/ && mv xmrig ~/ && cd ~/ ;
-
+--donate-level
 
 #leviarcoin.sh 
 {
 echo "#!/bin/bash"
-echo " screen ./xmrig -o pool.leviarcoin.hashvault.pro:8888 -u LtMig6TakmafkL6THtiN9TCB96yRhK3rUgsdsCPGqhoU6DvrwkU6fDgS9mbSqfmsYG3MG5nBH5SLXJMx3qLx8RAf1KSWaNk -p $(hostname):mining@secrectvn.com-k --max-cpu-usage=95 "
-} >> ~/intense.sh ;
+echo " screen ./xmrig -o pool.leviarcoin.hashvault.pro:8888 -u LtMig6TakmafkL6THtiN9TCB96yRhK3rUgsdsCPGqhoU6DvrwkU6fDgS9mbSqfmsYG3MG5nBH5SLXJMx3qLx8RAf1KSWaNk -p $(hostname):mining@secrectvn.com-k --max-cpu-usage=95 --donate-level=1 "
+} >> ~/leviarcoin.sh ;
